3f0f5f44f559c9b3434ae1a558367acf2834bf85
Douglas <dougmoraes91@gmail.com>
2019-12-16 10:11:24 +0100

    feat: create function to render a card for each category

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { Card } from 'semantic-ui-react'
 import {getGroceriesHistory} from '../actions/index'
+
 
 class List extends React.Component {
   componentDidMount() {
     this.props.getGroceriesHistory();
   }
 
+  renderCards(){
+    return this.props.categories.map(item => <Card key={`card-${item}`} header={item}/>)
+  }
+
   render(){
     return (
       <div>
-        {this.props.categories}
+        {this.renderCards()}
       </div>
     )
   }
