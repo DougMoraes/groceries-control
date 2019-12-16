923041e652a439e3d87573182c9ed095e3cf3c7d
Douglas <dougmoraes91@gmail.com>
2019-12-16 21:03:07 +0100

    feat: create selectCategory action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,15 @@
 import groceriesAPI from '../api-config'
 import {
-  GET_GROCERIES_HISTORY
+  GET_GROCERIES_HISTORY,
+  SELECT_CATEGORY
 } from './types';
 
 export const getGroceriesHistory = () => async dispatch => {
   const response = await groceriesAPI.get('/groceries');
 
   dispatch({ type: GET_GROCERIES_HISTORY, payload: response.data });
+}
+
+export const selectCategory = (category) => {
+  return { type: SELECT_CATEGORY, payload: category };
 }
