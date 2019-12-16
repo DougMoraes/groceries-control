c96e918dbc5625e262f050c259ef124bd4d04b40
Douglas <dougmoraes91@gmail.com>
2019-12-16 21:03:46 +0100

    feat: import selectCategory state

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { Card } from 'semantic-ui-react'
-import {getGroceriesHistory} from '../actions/index'
-
+import { 
+  getGroceriesHistory,
+  selectCategory,
+ } from '../actions/index'
 
 class List extends React.Component {
+
   componentDidMount() {
     this.props.getGroceriesHistory();
   }
 
+  onClickHandler = (data) => {
+    this.props.selectCategory(data.target.innerText)
+  }
+
   renderCards(){
-    return this.props.categories.map(item => <Card key={`card-${item}`} header={item}/>)
+    return this.props.categories.map(item => <Card key={`card-${item}`} onClick={this.onClickHandler} header={item}/>)
   }
 
   render(){
@@ -25,8 +32,12 @@ class List extends React.Component {
 const mapStateToProps = state => {
   return {
     groceries: state.groceries,
-    categories: [...new Set(state.groceries.map(item => item.category))]
+    categories: [...new Set(state.groceries.map(item => item.category))],
+    selectedCategory: state.selectCategory
   };
 }
 
-export default connect(mapStateToProps, { getGroceriesHistory })(List);
+export default connect(mapStateToProps, { 
+  getGroceriesHistory,
+  selectCategory
+ })(List);
