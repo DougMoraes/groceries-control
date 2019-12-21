7feccd391d735c5f516b233fc01b252c6cf2143e
Douglas <dougmoraes91@gmail.com>
2019-12-21 17:40:13 +0100

    feat: add functions to ccalculate average price

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -16,14 +16,60 @@ class List extends React.Component {
     this.props.selectCategory(data.target.innerText)
   }
 
-  renderCards(){
-    return this.props.categories.map(item => <Card key={`card-${item}`} onClick={this.onClickHandler} header={item}/>)
+  getRegistriesPerCategory(category){
+    return this.props.groceries.filter(registry => registry.category === category);
+  }
+
+  sumUpReducer(initialValue, registry){
+    initialValue.qtd += parseFloat(registry.quantity)
+    initialValue.price += parseFloat(registry.price)
+
+    return initialValue;
+  }
+
+  calculateAveragePrice(defaultQtd, qtd, price){
+    let calculatedQtd = qtd / defaultQtd; 
+
+    return price / calculatedQtd;
+  }
+
+  getAveragePrice(category){
+    let registries = this.getRegistriesPerCategory(category);
+    let unitMeasurement = registries[0].unitMeasurement
+    let initialValue = { qtd: 0, price: 0 };
+    let valuesSumUp = registries.reduce(this.sumUpReducer, initialValue);
+    let defaultQtd = 1;
+    
+    switch (unitMeasurement){
+      case 'Gr':
+        defaultQtd = 100;
+        return `‎€ ${this.calculateAveragePrice(defaultQtd, valuesSumUp.qtd, valuesSumUp.price).toFixed(2)} per ${defaultQtd} gr`
+
+      case 'Ml':
+        defaultQtd = 100;
+        return `‎€ ${this.calculateAveragePrice(defaultQtd, valuesSumUp.qtd, valuesSumUp.price).toFixed(2)} per ${defaultQtd} ml`
+
+      default:
+        return `‎€ ${ this.calculateAveragePrice(defaultQtd, valuesSumUp.qtd, valuesSumUp.price).toFixed(2)} per ${defaultQtd + unitMeasurement} `
+    }
+  }
+
+  renderCategoryCards(){
+    return this.props.categories.map(category => {
+
+      return <Card 
+      key={`card-${category}`} 
+      onClick={this.onClickHandler} 
+      header={category} 
+      description={this.getAveragePrice(category)}
+      />
+    })
   }
 
   render(){
     return (
       <div>
-        {this.renderCards()}
+        {this.renderCategoryCards()}
       </div>
     )
   }
