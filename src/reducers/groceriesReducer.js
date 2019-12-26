081c4545254897392e0ef1436c1ec2e2930db52d
Douglas <dougmoraes91@gmail.com>
2019-12-26 17:14:34 +0100

    feat: add ADD_GROCERIES_REGISTRY action behavior on groceriesReducer

--- src/reducers/groceriesReducer.js
+++ src/reducers/groceriesReducer.js
@@ -1,11 +1,14 @@
 import {
-  GET_GROCERIES_HISTORY
+  GET_GROCERIES_HISTORY,
+  ADD_GROCERIES_REGISTRY
 } from '../actions/types';
 
 export default (groceries = [], action) => {
   switch (action.type) {
     case GET_GROCERIES_HISTORY:
-      return action.payload ? action.payload : [] ; 
+      return action.payload ? action.payload : [] ;
+    case ADD_GROCERIES_REGISTRY:
+      return action.payload ; 
     default:
       return groceries;
   }
