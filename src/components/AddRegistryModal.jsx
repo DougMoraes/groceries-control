cace342857b99547408bb3b6e3b1287bce61efaa
Douglas <dougmoraes91@gmail.com>
2019-12-26 15:39:05 +0100

    feat: create form to add new registry

--- src/components/AddRegistryModal.jsx
+++ src/components/AddRegistryModal.jsx
@@ -1,25 +1,93 @@
 import React from 'react';
-import { Modal, Header, Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { Button, Form, Modal, Radio, Select } from 'semantic-ui-react';
 
 class AddRegistryModal extends React.Component {
+
+  renderDatalist(id){
+    return (<datalist id={id}>{this.props[id].map(item => <option value={item}/>)}</datalist>)
+  }
+
+  prepareOptions(list){
+    return list.map(item => {return {key: item, value: item, text: item}})
+  }
 
   render() {
     return (
       <Modal trigger={<Button circular icon='plus' style={{margin: "10px", alignSelf: "flex-start" }}/>}>
-        <Modal.Header>Select a Photo</Modal.Header>
-        <Modal.Content image>
-          <Modal.Description>
-            <Header>Default Profile Image</Header>
-            <p>
-              We've found the following gravatar image associated with your e-mail
-              address.
-          </p>
-            <p>Is it okay to use this photo?</p>
-          </Modal.Description>
-        </Modal.Content>
-      </Modal>
+        <Modal.Header>Add registry</Modal.Header>
+          <Modal.Content>
+            <Modal.Description>
+              <Form>
+                <Form.Group widths='equal'>
+                  <Form.Field>
+                    <label>Category</label>
+                    <input placeholder='Category' list="categories" />
+                    {this.renderDatalist('categories')}
+                  </Form.Field>
+                  <Form.Field>
+                    <label>Product</label>
+                    <input placeholder='Product' list="products" />
+                    {this.renderDatalist('products')}
+                  </Form.Field>
+                </Form.Group>
+                <Form.Group widths='equal'>
+                  <Form.Field>
+                    <label>Category</label>
+                    <input placeholder='Category' list="categories" />
+                    {this.renderDatalist('categories')}
+                  </Form.Field>
+                  <Form.Field>
+                    <label>Product</label>
+                    <input placeholder='Product' list="products" />
+                    {this.renderDatalist('products')}
+                  </Form.Field>
+                </Form.Group>
+                <Form.Group widths='equal'>
+                  <Form.Field>
+                    <label>Quantity</label>
+                    <input type="number" placeholder='Quantity' step={0.01} />
+                  </Form.Field>
+                  <Form.Field>
+                    <label>Unit of Measurement</label>
+                    <Select placeholder='Unit of Measurement' options={this.prepareOptions(this.props.units)} />
+                  </Form.Field>
+                  <Form.Field>
+                    <label>Price</label>
+                    <input type="number" placeholder='Price' step={0.01} />
+                  </Form.Field>
+                </Form.Group>
+                <Form.Group widths='equal'>
+                  <Form.Field>
+                    <label>Market</label>
+                    <Select placeholder='Markets' options={this.prepareOptions(this.props.markets)} />
+                  </Form.Field>
+                  <Form.Field>
+                    <label>Date</label>
+                    <input type='date' placeholder='Date' />
+                  </Form.Field>
+                  <Form.Field>
+                    <label>On Sale</label>
+                    <Radio toggle />
+                  </Form.Field>
+                </Form.Group>
+                <Button type="submit">Submit</Button>
+              </Form>
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
     )
   }
 }
 
-export default AddRegistryModal;
+const mapStateToProps = state => {
+  return {
+    products: [...new Set(state.groceries.map(item => item.product))],
+    categories: [...new Set(state.groceries.map(item => item.category))],
+    markets: [...new Set(state.groceries.map(item => item.market))],
+    units: [...new Set(state.groceries.map(item => item.unitMeasurement))],
+    selectedCategory: state.selectedCategory
+  };
+}
+
+export default connect(mapStateToProps, {})(AddRegistryModal);
