0588682054c854a05f315c24f33fc90facf83394
Douglas <dougmoraes91@gmail.com>
2020-01-03 09:07:31 +0100

    feat: implement form reducer from redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 import groceriesReducer from './groceriesReducer';
 import categoryReducer from './categoryReducer';
 
 export default combineReducers({
   groceries: groceriesReducer,
-  selectedCategory: categoryReducer
+  selectedCategory: categoryReducer,
+  form: form
 })
